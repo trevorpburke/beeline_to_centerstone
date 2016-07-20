44415f4def58b7deca4d25ae5d08c1042c1a50d1
Trevor Burke <trevorpburke@gmail.com>
2016-07-20 10:28:43 -0700

    change columns name

--- script.py
+++ script.py
@@ -11,20 +11,27 @@ from tkFileDialog import askopenfilename, asksaveasfilename
 
 # GUI windows 
 Tk().withdraw() # prevents the root window from appearing
-filename = askopenfilename(initialdir ='~') # show an "Open" dialog box and return the path to the selected file
+filename = askopenfilename(initialdir ='~/Downloads') # show an "Open" dialog box and return the path to the selected file
 timestr = time.strftime("%m-%d-%Y")
 
-save_file = asksaveasfilename(initialdir='~', initialfile='contractors_' + timestr)
+save_file = asksaveasfilename(initialdir='~/Documents/beeline_reports', initialfile='onboard_' + timestr)
 
 # deleting, modifying, and inserting column data 
 df = pd.read_excel(filename)
 
 df = df.drop(df.columns[[6, 9, 10]], axis=1)
-df.rename(columns={'Beeline ID': 'Employee ID', 'Date': 'Hire Date', 'Date.1': 'End Date',
-                    'Location Name': 'Business Site', 'Hiring Manager': 'Workers Manager',
-                    'Department Name': 'Supervisory Org'}, inplace=True)
+
+df.rename(columns={'Beeline ID': 'Employee ID',
+                    'Contingent Worker First Name': 'First Name',
+                    'Contingent Worker Last Name': 'Last Name'
+                    'Date': 'Hire Date', 
+                    'Date.1': 'End Date',
+                    'Location Name': 'Business Site', 
+                    'Assignment Manager': 'Workers Manager',
+                    'Billing Department Name': 'Supervisory Org'}, inplace=True)
 
 df.insert(loc=8, column='Employee Type Name', value='Contractor')
 
 # save to Desktop 
 df.to_csv(save_file, sep='\t', index=False)
+
